e1f3b9d1b4052c582967326bc47034bc7fabf953
João Ramajo <joaoramajo744@gmail.com>
2026-05-02 13:57:06 -0300

    fix: corrigir sobreposicao acidental no grid

--- src/utils/gallery.ts
+++ src/utils/gallery.ts
@@ -35,15 +35,15 @@ export function getGalleryTileShape(ratio: number): TattooLayoutHint {
 export function getGalleryTileClasses(shape: TattooLayoutHint): string {
   switch (shape) {
     case "wide":
-      return "aspect-[16/10] lg:col-span-7 lg:row-span-3";
+      return "aspect-[16/10] lg:col-span-7 lg:row-span-3 lg:h-full lg:aspect-auto";
     case "landscape":
-      return "aspect-[4/3] lg:col-span-6 lg:row-span-3";
+      return "aspect-[4/3] lg:col-span-6 lg:row-span-3 lg:h-full lg:aspect-auto";
     case "square":
-      return "aspect-square lg:col-span-4 lg:row-span-4";
+      return "aspect-square lg:col-span-4 lg:row-span-4 lg:h-full lg:aspect-auto";
     case "tall":
-      return "aspect-[3/5] lg:col-span-3 lg:row-span-5";
+      return "aspect-[3/5] lg:col-span-3 lg:row-span-5 lg:h-full lg:aspect-auto";
     case "portrait":
     default:
-      return "aspect-[4/5] lg:col-span-4 lg:row-span-5";
+      return "aspect-[4/5] lg:col-span-4 lg:row-span-5 lg:h-full lg:aspect-auto";
   }
 }
